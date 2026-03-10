856c164a42638c05ce69fd6a6eb7c3094501fb21
Juansao Fortunio Tandi <juansao.fortunio@ui.ac.id>
2026-03-10 14:31:57 +0700

    [GREEN] Implement logic for Order repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -2,19 +2,32 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class OrderRepository {
+    private final Map<String, Order> orderData = new HashMap<>();
+
     public Order save(Order order) {
-        return null; // Skeleton
+        orderData.put(order.getId(), order);
+        return order;
     }
 
     public Order findById(String id) {
-        return null; // Skeleton
+        return orderData.get(id);
     }
 
     public List<Order> findAllByAuthor(String author) {
-        return null; // Skeleton
+        List<Order> result = new ArrayList<>();
+        for (Order order : orderData.values()) {
+            if (order.getAuthor().equals(author)) {
+                result.add(order);
+            }
+        }
+        return result;
     }
 }
