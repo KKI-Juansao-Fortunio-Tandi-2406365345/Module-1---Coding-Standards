6fc2af5f5f1c9cf5b823d37cf63a660857b7bab3
Juansao Fortunio Tandi <juansao.fortunio@ui.ac.id>
2026-03-10 15:50:51 +0700

    [GREEN] Implement Payment controller and UI templates for Bonus 1

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/PaymentController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/PaymentController.java
@@ -1,9 +1,13 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/payment")
@@ -12,11 +16,24 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @GetMapping("/addVoucher")
-    public String addVoucherPage() { return null; }
+    public String addVoucherPage(@RequestParam String orderId, Model model) {
+        model.addAttribute("orderId", orderId);
+        return "addPaymentVoucher";
+    }
 
     @GetMapping("/addCod")
-    public String addCodPage() { return null; }
+    public String addCodPage(@RequestParam String orderId, Model model) {
+        model.addAttribute("orderId", orderId);
+        return "addPaymentCod";
+    }
 
     @PostMapping("/add")
-    public String addPayment() { return null; }
+    public String addPayment(@RequestParam String orderId,
+                             @RequestParam String method,
+                             @RequestParam Map<String, String> allRequestParams) {
+        // Spring packs all params into the map; we just need the paymentData parts
+        Payment payment = new Payment(orderId, method, allRequestParams);
+        paymentService.addPayment(payment);
+        return "redirect:/order/list";
+    }
 }
