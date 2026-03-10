8cf3cd22948f637fa57f4a05b80c5ce768f613aa
Juansao Fortunio Tandi <juansao.fortunio@ui.ac.id>
2026-03-10 14:58:45 +0700

    [RED] Add tests and skeleton for Payment repository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PaymentRepository {
+    public Payment save(Payment payment) {
+        return null; // Skeleton
+    }
+
+    public Payment findById(String id) {
+        return null; // Skeleton
+    }
+}
