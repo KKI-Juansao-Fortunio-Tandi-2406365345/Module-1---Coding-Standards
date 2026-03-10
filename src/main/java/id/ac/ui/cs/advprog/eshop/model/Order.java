609e6dd84a76c9bd0f4979abf7a4904c7edd6cf3
Juansao Fortunio Tandi <juansao.fortunio@ui.ac.id>
2026-03-10 14:27:34 +0700

    [REFACTOR] Use OrderStatus enum in Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import lombok.Getter;
-import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -13,7 +13,7 @@ public class Order {
     String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
-        this(id, products, orderTime, author, "WAITING_PAYMENT");
+        this(id, products, orderTime, author, OrderStatus.WAITING_PAYMENT.getValue());
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
@@ -29,8 +29,7 @@ public class Order {
     }
 
     public void setStatus(String status) {
-        String[] statusList = {"WAITING_PAYMENT", "SUCCESS", "CANCELLED", "FAILED"};
-        if (Arrays.asList(statusList).contains(status)) {
+        if (OrderStatus.contains(status)) {
             this.status = status;
         } else {
             throw new IllegalArgumentException();
