d90f634c047f5c7387c4ba5ac0588d2158d6fd59
Juansao Fortunio Tandi <juansao.fortunio@ui.ac.id>
2026-03-10 14:59:48 +0700

    [GREEN] Implement logic for Payment repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -2,14 +2,19 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
+    private final Map<String, Payment> paymentData = new HashMap<>();
+
     public Payment save(Payment payment) {
-        return null; // Skeleton
+        paymentData.put(payment.getId(), payment);
+        return payment;
     }
 
     public Payment findById(String id) {
-        return null; // Skeleton
+        return paymentData.get(id);
     }
 }
