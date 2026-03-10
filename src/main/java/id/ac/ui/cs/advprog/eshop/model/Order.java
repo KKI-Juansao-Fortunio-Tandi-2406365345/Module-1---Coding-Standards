7871594544fa8af23bfabeaddd0e29ec2c2547cb
Juansao Fortunio Tandi <juansao.fortunio@ui.ac.id>
2026-03-10 14:22:15 +0700

    [RED] Add Order model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+public class Order {
+    String id;
+    List<Product> products;
+    Long orderTime;
+    String author;
+    @Setter
+    String status;
+
+    public Order(String id, List<Product> products, Long orderTime, String author) {
+        // Leave empty for now
+    }
+
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        // Leave empty for now
+    }
+}
