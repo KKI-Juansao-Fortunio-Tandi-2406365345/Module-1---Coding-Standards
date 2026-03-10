a7654be58045314e5fbf368fa925a4f6616eb58c
Juansao Fortunio Tandi <juansao.fortunio@ui.ac.id>
2026-03-10 14:41:37 +0700

    [RED] Unit test for Payment model (Voucher)

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,46 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        this.paymentData = new HashMap<>();
+    }
+
+    @Test
+    void testCreatePaymentVoucherCodeSuccess() {
+        paymentData.put("voucherCode", "ESHOP12345678ABC");
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER_CODE", paymentData);
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentVoucherCodeRejectedInvalidLength() {
+        paymentData.put("voucherCode", "ESHOP12345678AB"); // 15 characters
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER_CODE", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentVoucherCodeRejectedNotStartWithEshop() {
+        paymentData.put("voucherCode", "NOTHP12345678ABC");
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER_CODE", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentVoucherCodeRejectedNotEightNumbers() {
+        paymentData.put("voucherCode", "ESHOP1234567AABC"); // Only 7 numbers
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER_CODE", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
+    }
+}
